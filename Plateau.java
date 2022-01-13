162fca6759e3277df573c096fd8bf203137df50c
RathierS <95344979+RathierS@users.noreply.github.com>
2022-01-13 12:25:41 +0100

    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -172,7 +172,7 @@ public class Plateau {
 
     public static void main(String[] args) {
 
-        // prblème avec int[][] plateau;
+        
         Plateau plateau = new Plateau();
         // Ut.afficher(plateau.toString());
         int[] nbPointsJet = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10, 10 };
